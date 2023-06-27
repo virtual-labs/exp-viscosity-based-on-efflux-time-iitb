86686867fbea472f537e563941f9ecb3facdd1d7
vleadadmin <admin@vlabs.ac.in>
2023-06-27 05:15:38 +0000

    https://virtual-labs.github.io/virtual-labs/exp-viscosity-based-on-efflux-time-iitb click on the link to test your code.

---
+++ simulation/js/loaded_images.js
@@ -0,0 +1,8 @@
+// all images
+var rect_tank = new Image();
+rect_tank.src = `./images/webp/Webp/RectangularTankaAttachedWithCapillary.webp`;
+var collecting_tank = new Image();
+collecting_tank.src = `./images/webp/Webp/CollectingTank.webp`;
+var timer = new Image();
+timer.src = `./images/webp/Webp/Timer.webp`;
+//# sourceMappingURL=loaded_images.js.map
